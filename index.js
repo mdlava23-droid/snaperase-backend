1bf6b5d42d9203bcd1421e6b36f6137d9849a086
mdlava23-droid <mdlava23@gmail.com>
2025-12-21 11:31:19 +0600

    Set up Express server with basic route

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
+
 const PORT = process.env.PORT;
 
 if (!PORT) {
   console.error("PORT not found");
   process.exit(1);
 }
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
